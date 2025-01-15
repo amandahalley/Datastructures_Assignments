f9933e9aedc2af8796cb0301e7e401a04fd39c03
ahllyy <amanda.halley@keyin.com>
2025-01-15 19:42:13 -0330

    Initial commit

---
+++ src/main/java/Arrays/QuestionTwo.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+//Simulate a movie theater that helps users to book and reserve seats.
+
+//1. reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+//2. cancel seats
+//3. retrieve initial seating charting
+
+public class QuestionTwo {
+}
