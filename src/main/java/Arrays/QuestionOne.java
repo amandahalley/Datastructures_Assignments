187e877a9411d3c421afa06e2edf890d9e56b1b7
ahllyy <amanda.halley@keyin.com>
2025-01-19 15:44:00 -0330

    insert method for recording temperatures by day

--- src/main/java/Arrays/QuestionOne.java
+++ src/main/java/Arrays/QuestionOne.java
@@ -10,13 +10,32 @@ import java.util.Arrays;
 
 
 public class QuestionOne {
-    int[] temperature = null;
+    int[] temperatures = null;
 
     // initialize array
     public QuestionOne(int numDays) {
-        temperature = new int[numDays];
-        for (int i = 0; i < temperature.length; i++) {
-            temperature[i] = Integer.MIN_VALUE;
+        temperatures = new int[numDays];
+        for (int i = 0; i < temperatures.length; i++) {
+            temperatures[i] = Integer.MIN_VALUE;
         }
     }
+
+    //insert temperatures into array
+    public void addTemperatures(int day, int temperature) {
+        //Check is day is initialized, records if not
+        try {
+            if (temperatures[day] == Integer.MIN_VALUE) {
+                temperatures[day] = temperature;
+                System.out.println("Temperature for: " + (day + 1) + " has been recorded.");
+            } else {
+                //notify user if temperature is already recorded
+               System.out.println("Temperature for: " + (day + 1) + " already exists." );
+            }
+            // error message for invalid date
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid date.");
+        }
+    }
+
+
 }
