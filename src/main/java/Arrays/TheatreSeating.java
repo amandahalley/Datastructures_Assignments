48c367c9386335b44d9ffe2dbd5efa1fb5ed2fe4
ahllyy <amanda.halley@keyin.com>
2025-01-19 16:18:22 -0330

    create constructor to initialize seating chart, setting each seat to indicate it is available

--- src/main/java/Arrays/TheatreSeating.java
+++ src/main/java/Arrays/TheatreSeating.java
@@ -7,6 +7,19 @@ package Arrays;
 //3. retrieve initial seating charting
 
 public class TheatreSeating {
+    int [][] arr = null;
 
+    //initialize movie theatre seating with given num of rows/cols
+    public TheatreSeating(int rows, int cols) {
+        //initialize 2d array
+        arr = new int[rows][cols];
+
+        //loop through each row, and then each column
+        for (int row = 0; row < arr.length; row++) {
+            for(int col = 0; col < arr[0].length; col++){
+                arr[row][col] = Integer.MIN_VALUE;
+            }
+        }
+    }
 
 }
