da04f6c6d21685aa23a5b05015e6b16e2bdc26fc
ahllyy <amanda.halley@keyin.com>
2025-02-01 15:48:48 -0330

    output clean up

--- src/main/java/DeleteMethod/Main.java
+++ src/main/java/DeleteMethod/Main.java
@@ -8,25 +8,25 @@ public class Main {
         sll.insertInLinkedList(3,3);
         sll.insertInLinkedList(4,4);
 
-        System.out.println("Complete List: ");
+        System.out.println("Complete List: "); //1 -> 2 -> 3 -> 4
         sll.traverseLinkedList();
-        sll.searchNode(4);
+        sll.searchNode(4); //Found the node: 4 at location: 3
 
 
         //delete method
         sll.deleteFromLinkedList(0);
-        System.out.print("Deleting index 0: ");
+        System.out.print("Deleting index 0: "); //2 -> 3 -> 4
         sll.traverseLinkedList();
 
         sll.deleteFromLinkedList(2);
-        System.out.print("Deleting index 2: ");
+        System.out.print("Deleting index 2: "); //2 -> 3
         sll.traverseLinkedList();
 
         sll.deleteFromLinkedList(1);
-        System.out.print("Deleting index 1: ");
+        System.out.print("Deleting index 1: "); //2
         sll.traverseLinkedList();
 
-        System.out.println("After all deletions: ");
+        System.out.println("\nList after all deletions: "); //2
         sll.traverseLinkedList();
     }
 }
