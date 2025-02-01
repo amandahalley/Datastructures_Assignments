84b3a5c5120a832934e0c61e4d626960bf0e8e4c
ahllyy <amanda.halley@keyin.com>
2025-02-01 14:20:33 -0330

    implemented linked list deletion
    -printedlist before and after each deletion and all

--- src/main/java/DeleteMethod/Main.java
+++ src/main/java/DeleteMethod/Main.java
@@ -8,7 +8,25 @@ public class Main {
         sll.insertInLinkedList(3,3);
         sll.insertInLinkedList(4,4);
 
+        System.out.println("Complete List: ");
         sll.traverseLinkedList();
         sll.searchNode(4);
+
+
+        //delete method
+        sll.deleteFromLinkedList(0);
+        System.out.print("Deleting index 0: ");
+        sll.traverseLinkedList();
+
+        sll.deleteFromLinkedList(2);
+        System.out.print("Deleting index 2: ");
+        sll.traverseLinkedList();
+
+        sll.deleteFromLinkedList(1);
+        System.out.print("Deleting index 1: ");
+        sll.traverseLinkedList();
+
+        System.out.println("After all deletions: ");
+        sll.traverseLinkedList();
     }
 }
