5b6b10ff9a059c5f329d1b1e3e843c5b80f7ebb2
ahllyy <amanda.halley@keyin.com>
2025-02-15 15:57:13 -0330

    intialized both dog and cat queues for managing oldest for adoption (fifo)

--- src/main/java/Queues/AnimalShelter.java
+++ src/main/java/Queues/AnimalShelter.java
@@ -1,4 +1,16 @@
 package Queues;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+//created queues for storing dogs and cats
 public class AnimalShelter {
+    private Queue<Cat> cats;
+    private Queue<Dog> dogs;
+
+    //linked list queues for dogs/cats
+    public AnimalShelter() {
+        cats = new LinkedList<>();
+        dogs = new LinkedList<>();
+    }
 }
