2c9fe0405582c1f55d8c6f6b2e0d68905ae969fb
ahllyy <amanda.halley@keyin.com>
2025-02-23 16:30:07 -0330

    created task class, for adding task details and marking task as complete

--- src/main/java/MidtermSprint/Task.java
+++ src/main/java/MidtermSprint/Task.java
@@ -1,4 +1,23 @@
 package MidtermSprint;
 
 public class Task {
+    String description;
+    Boolean isCompleted;
+
+    //constructor - task set to not complete
+    public Task(String description){
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    //for marking task as complete
+    public void markedComplete() {
+        isCompleted = true;
+    }
+
+    @Override
+    public String toString() {
+        return description + " - " + (isCompleted ? " Completed" : "Incomplete");
+    }
 }
+
