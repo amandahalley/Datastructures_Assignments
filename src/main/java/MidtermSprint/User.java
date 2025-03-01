3fbf042461938620c78bded0bc36e06d38e0bb05
ahllyy <amanda.halley@keyin.com>
2025-02-28 20:42:36 -0330

    created User class for creating the user and adding tasks under the specific user, marking them as complete and getting a list of all tasks under specefied user

--- src/main/java/MidtermSprint/User.java
+++ src/main/java/MidtermSprint/User.java
@@ -1,4 +1,30 @@
 package MidtermSprint;
 
-public class User {
+class User {
+    String name;
+    TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public void addTask(String taskDescription) {
+        taskList.addTask(taskDescription);
+        System.out.println("Task added for " + name + ": " + taskDescription);
+    }
+
+    public void markAsCompleted(int index) {
+        if (taskList.markAsComplete(index)) {
+            System.out.println("Task " + index + " marked as completed for " + name);
+        } else {
+            System.out.println("Invalid task index for " + name);
+        }
+    }
+
+    public void printTasks() {
+        System.out.println("\nTasks for " + name + ":");
+        taskList.printTasks();
+    }
 }
+
